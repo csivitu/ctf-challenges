7b848a81378bd8b823404e70751097f77128bb46
alias-rahil <59060219+alias-rahil@users.noreply.github.com>
2020-06-30 17:50:41 +0530

    fix: rename files and env variables

--- ctfd.py
+++ ctfd.py
@@ -1,11 +1,11 @@
 import os
-TOKEN = os.getenv("TOKEN", default=None)
-URL = os.getenv("URL", default=None)
-os.system(f"echo '{URL}\n{TOKEN}\ny' | ctf init")
+CTFD_TOKEN = os.getenv("CTFD_TOKEN", default=None)
+CTFD_URL = os.getenv("V_URL", default=None)
+os.system(f"echo '{CTFD_URL}\n{CTFD_TOKEN}\ny' | ctf init")
 blacklist = [".git", ".github", ".ctf"]
 categories = [name for name in os.listdir(".") if os.path.isdir(name) and name not in blacklist]
 for i in categories:
     challenges = [f"{i}/{name}" for name in os.listdir(f"./{i}") if os.path.isdir(f"{i}/{name}")]
     for j in challenges:
         if os.path.exists(f"{j}/challenge.yml"):
-            os.system(f"ctf challenge sync {j}; ctf challenge install {j}")
+            os.system(f"ctf challenge sync {j}; ctf challenge install {j}; ")
