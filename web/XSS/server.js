88bc95938bfd95c0205f74032e80c2e0cd54b61a
Rohan Mukherjee <rohanbayern@gmail.com>
2020-07-01 21:15:47 +0530

    feat: Modified XSS script

--- web/XSS/server.js
+++ web/XSS/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const hbs = require('express-handlebars');
 
 const app = express();
 
-app.use
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', hbs());
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
     res.render();
