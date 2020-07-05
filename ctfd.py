807ba21eb40b0a9e7d307c4ebdfb3980ba44ec0e
Rohan Mukherjee <rohanbayern@gmail.com>
2020-07-05 11:33:54 +0530

    feat: Ignore all  folders

--- ctfd.py
+++ ctfd.py
@@ -1,18 +1,28 @@
 import os
+import re
+
 CTFD_TOKEN = os.getenv("CTFD_TOKEN", default=None)
 CTFD_URL = os.getenv("CTFD_URL", default=None)
+
+if not CTFD_TOKEN or not CTFD_URL:
+    exit(1)
+
 os.system(f"echo '{CTFD_URL}\n{CTFD_TOKEN}\ny' | ctf init")
-blacklist = [".git", ".github", ".ctf"]
-categories = [name for name in os.listdir(".") if os.path.isdir(name) and name not in blacklist]
 
-print("Categories:")
-print("\n".join(categories))
+denylist_regex = r'\..*'
+categories = [name for name in os.listdir(".") if os.path.isdir(name) and not re.match(denylist_regex, name)]
 
-for i in categories:
-    challenges = [f"{i}/{name}" for name in os.listdir(f"./{i}") if os.path.isdir(f"{i}/{name}")]
-    print(f"Category {i}:")
-    print("\n".join(challenges))
-    for j in challenges:
-        print(f"Syncing challenge {j}")
-        if os.path.exists(f"{j}/challenge.yml"):
-            os.system(f"ctf challenge sync '{j}'; ctf challenge install '{j}'")
+print("Categories: " + ", ".join(categories))
+
+for category in categories:
+    challenges = [f"{category}/{name}" for name in os.listdir(f"./{category}") if os.path.isdir(f"{category}/{name}")]
+    print(f"\nCategory {category}:")
+    if challenges != []:
+        print("\n".join(challenges))
+    else:
+        print("None")
+
+    for challenge in challenges:
+        if os.path.exists(f"{challenge}/challenge.yml"):
+            print(f"Syncing challenge: {challenge}")
+            os.system(f"ctf challenge sync '{challenge}'; ctf challenge install '{challenge}'")
