2f73ec174279f3c8613afc485d0890e82984c7d7
Rohan Mukherjee <rohanbayern@gmail.com>
2020-07-09 14:42:12 +0530

    feat: Each challenge in it's own thread

--- ctfd.py
+++ ctfd.py
@@ -1,28 +1,50 @@
 import os
 import re
+import threading
 
-CTFD_TOKEN = os.getenv("CTFD_TOKEN", default=None)
-CTFD_URL = os.getenv("CTFD_URL", default=None)
+# Initialize ctfcli with the CTFD_TOKEN and CTFD_URL.
+def init():
+    CTFD_TOKEN = os.getenv("CTFD_TOKEN", default=None)
+    CTFD_URL = os.getenv("CTFD_URL", default=None)
 
-if not CTFD_TOKEN or not CTFD_URL:
-    exit(1)
+    if not CTFD_TOKEN or not CTFD_URL:
+        exit(1)
 
-os.system(f"echo '{CTFD_URL}\n{CTFD_TOKEN}\ny' | ctf init")
+    os.system(f"echo '{CTFD_URL}\n{CTFD_TOKEN}\ny' | ctf init")
 
-denylist_regex = r'\..*'
-categories = [name for name in os.listdir(".") if os.path.isdir(name) and not re.match(denylist_regex, name)]
 
-print("Categories: " + ", ".join(categories))
+# Each category is in it's own directory, get the names of all directories that do not begin with '.'.
+def get_categories():
+    denylist_regex = r'\..*'
 
-for category in categories:
+    categories = [name for name in os.listdir(".") if os.path.isdir(name) and not re.match(denylist_regex, name)]
+    print("Categories: " + ", ".join(categories))
+    return categories
+
+
+# Synchronize all challenges in the given category, where each challenge is in it's own folder.
+def sync(category):
     challenges = [f"{category}/{name}" for name in os.listdir(f"./{category}") if os.path.isdir(f"{category}/{name}")]
-    print(f"\nCategory {category}:")
-    if challenges != []:
-        print("\n".join(challenges))
-    else:
-        print("None")
 
     for challenge in challenges:
         if os.path.exists(f"{challenge}/challenge.yml"):
             print(f"Syncing challenge: {challenge}")
             os.system(f"ctf challenge sync '{challenge}'; ctf challenge install '{challenge}'")
+
+
+# Synchronize each category in it's own thread.
+if __name__ == "__main__":
+    init()
+    categories = get_categories()
+
+    jobs = []
+    for category in categories:
+        jobs.append(threading.Thread(target=sync, args=(category, )))
+    
+    for job in jobs:
+        job.start()
+
+    for job in jobs:
+        job.join()
+
+    print("Synchronized successfully!")
