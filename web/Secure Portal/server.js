3c9f55aa1cae28332ee55a152f8d91f64169ed3c
Rohan Mukherjee <rohanbayern@gmail.com>
2020-07-11 13:21:16 +0530

    feat: Added '/static' to express.static

--- web/Secure Portal/server.js
+++ web/Secure Portal/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
-app.use(express.static('public'));
+app.use('/static', express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index');
