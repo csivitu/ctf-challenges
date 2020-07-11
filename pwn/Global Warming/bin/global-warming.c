8ff4b00b14879a0a4f497b1f9956f3c635ab9bf2
Rohan Mukherjee <rohanbayern@gmail.com>
2020-07-11 13:17:08 +0530

    feat: Modified format string to take input

--- pwn/Global Warming/bin/global-warming.c
+++ pwn/Global Warming/bin/global-warming.c
@@ -1,27 +1,31 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int admin = 0;
 
-void login(char username[10],char* pass)
+void login(char username[10], char *pass)
 {
 
 	printf(pass);
-	if(admin != 0)
+	if (admin != 0)
 	{
 		system("cat flag.txt");
 	}
-	else 
+	else
 	{
 		printf("You cannot login as admin.");
 	}
 }
 
-int main(int argc, char **argv)
+int main()
 {
 	setbuf(stdout, NULL);
-        setbuf(stdin, NULL);
-        setbuf(stderr, NULL);
-	login("User",argv[1]);
+	setbuf(stdin, NULL);
+	setbuf(stderr, NULL);
+
+	char pass[1024];
+	fgets(pass, sizeof(pass), stdin);
+
+	login("User", pass);
 }
