a10b271032bcae723676e90b7e40f54c82475cf6
thebongy <rishit.bansal0@gmail.com>
2020-07-14 15:07:40 +0530

    fix: Fix dockerfile perms, make small changes to make chall more realistic

--- web/The Usual Suspects/server.py
+++ web/The Usual Suspects/server.py
@@ -3,22 +3,45 @@ import tornado.ioloop
 import tornado.web
 import os, pwd, grp
 
+def drop_privileges(uid_name='ctf', gid_name='ctf'):
+    running_uid = pwd.getpwnam(uid_name).pw_uid
+    running_gid = grp.getgrnam(gid_name).gr_gid
+
+    os.setgroups([])
+
+    os.setgid(running_gid)
+    os.setuid(running_uid)
+
+    old_umask = os.umask(0o077)
+
+
+flag = open('rf.txt').read()
+secret = open('cs.txt').read()
+
 TEMPLATE = '''
 <html>
  <head><title> 🐱‍👤Hello Hacker</title></head>
- <body style="background: #00FFFF">
- <h1 style="text-align:center; font-size:5rem;"> 🐱‍👤 The Usual Suspects 🐱‍👤 </h1>
+ <body style="background: #00FFFF;text-align:center;">
+ <h1 style="font-size:5rem;"> 🐱‍👤 The Usual Suspects 🐱‍👤 </h1>
  <br />
- <h2 style="text-align:center; font-size:4rem;"> You Have Arrived FOO ! </h2>
+ <h2 style="font-size:4rem;"> You Have Arrived! </h2>
  <br />
- <p style="text-align:center; font-size:2rem;"> 
- It's so good to see you here. I am your webpage. 
- I think you don't know what I actually look like :) <br/>
+ <p style="font-size:2rem;"> 
+ <p>Hey, wanna know how I rate some ice-cream flavours?</p>
+ <form method="GET"  action='/'>
+  <select id="icecream" name="icecream">
+      <option value="{{!chocolate}}">chocolate</option>
+      <option value="{{!vanilla}}">vanilla</option>
+      <option value="{{!butterscotch}}">butterscotch</option>
+  </select>
+  <input type="submit"></input>
+ </form>
+ <p>FOO</p>
  </p>
  <br/> <br/>
  <p style="text-align:center; font-size:2rem;">
- Oh I heard you're looking for my secret. Here's a little hint: <br/>
- My favourite Ice Cream flavour is <strong>Cookies&Cream</strong>
+ Oh I heard you're looking for my secret. <br>
+ <b>{{secret}}</b>
  </p>
  </body>
 </html>
@@ -27,43 +50,27 @@ TEMPLATE = '''
 class MainHandler(tornado.web.RequestHandler):
  
     def get(self):
-        person= {'name':"", 'secret':"csictf{my_secret_is_not_out}"}
+        chocolate = "My fav!! 10/10, would eat again"
+        vanilla = 'Vanilla is good too, but could be better, i give it a 7/10'
+        butterscotch = 'Yuck, who even likes that. 1/10'
         #if self.get_argument('name')==True:
-        person['name']= self.get_argument('name','')
-        template_data = TEMPLATE.replace("FOO",person['name'])
+        template_data = TEMPLATE.replace("FOO",self.get_argument('icecream',''))
         t = tornado.template.Template(template_data)
-        self.set_secure_cookie("mycookie", "myvalue")
-        self.write(t.generate(person=person,application=application))
-        
-        if self.get_secure_cookie("mycookie")==b"youwin":
-            self.write(open("rf.txt").read())
-        else:
-            self.write("Better luck next time!")
-        cookie=self.get_secure_cookie("mycookie")
-        print(cookie)
-        
+        self.set_secure_cookie("admin", "false")
+        secret = "Unfortunately, you aren't worthy"
+        if self.get_secure_cookie("admin")==b"true":
+            secret = flag
+        self.write(t.generate(chocolate=chocolate,vanilla=vanilla, butterscotch=butterscotch, application=application, secret=secret))
 
 
 
 application = tornado.web.Application([
-    (r"/test", MainHandler)
-    
-], debug=True, static_path=None, template_path=None, cookie_secret=open("cs.txt").read()
- 
+    (r"/", MainHandler)
+], debug=True, static_path=None, template_path=None, cookie_secret=secret)
+
 if __name__ == '__main__':
+    drop_privileges()
     application.listen(8000)
+    print("Listening :)")
     tornado.ioloop.IOLoop.instance().start()
 
-def drop_privileges(uid_name='nobody', gid_name='nogroup'):
-    if os.getuid() != 0:
-        return
-
-    running_uid = pwd.getpwnam(uid_name).pw_uid
-    running_gid = grp.getgrnam(gid_name).gr_gid
-
-    os.setgroups([])
- 
-    os.setgid(running_gid)
-    os.setuid(running_uid)
-
-    old_umask = os.umask(077)
