3843b5a7c5a4d377ebf7de5ccc44429ef4b0f59e
Rohan Mukherjee <rohanbayern@gmail.com>
2020-07-14 21:14:35 +0530

    feat: Added challenge wc

---
+++ web/wc/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>wc as a service</title>
+    <style>
+        html, body {
+            overflow: none;
+            max-height: 100vh;
+        }
+    </style>
+</head>
+
+<body style="height: 100vh; text-align: center; background-color: black; color: white; display: flex; flex-direction: column; justify-content: center;">
+    <h1>Character Count as a Service</h1>
+    <form>
+        <textarea style="border-radius: 1rem;" type="text" name="text" rows=30 cols=100></textarea><br />
+        <input type="submit">
+    </form>
+    <?php
+    ini_set('max_execution_time', 5);
+    if (isset($_GET["text"])) {
+        $text = $_GET["text"];
+
+        echo "<h2>The Character Count is: " . exec('echo \'' . $text . '\' | wc -c') . "</h2>";
+    }
+    ?>
+</body>
+
+</html>
