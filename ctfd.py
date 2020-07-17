4b9c7031a7af4d45aa3d9f69262d23eb11b31494
Rohan Mukherjee <rohanbayern@gmail.com>
2020-07-17 21:44:17 +0530

    test: Temp commit

--- ctfd.py
+++ ctfd.py
@@ -37,56 +37,74 @@ def change_state(waves, state):
 
     return visible
 
-# Initialize ctfcli with the CTFD_TOKEN and CTFD_URL.
-def init():
-    CTFD_TOKEN = os.getenv("CTFD_TOKEN", default=None)
-    CTFD_URL = os.getenv("CTFD_URL", default=None)
+def firewall(visible):
+    print(visible)
 
-    if not CTFD_TOKEN or not CTFD_URL:
-        exit(1)
+    create = """
+    gcloud compute firewall-rules create chall-name \
+    --allow tcp:<port no>
+    --priority 1000 \
+    --target-tags challs
+    """
 
-    os.system(f"echo '{CTFD_URL}\n{CTFD_TOKEN}\ny' | ctf init")
-
-
-# Each category is in it's own directory, get the names of all directories that do not begin with '.'.
-def get_categories():
-    denylist_regex = r'\..*'
-
-    categories = [name for name in os.listdir(".") if os.path.isdir(name) and not re.match(denylist_regex, name)]
-    print("Categories: " + ", ".join(categories))
-    return categories
-
-
-# Synchronize all challenges in the given category, where each challenge is in it's own folder.
-def sync(category):
-    challenges = [f"{category}/{name}" for name in os.listdir(f"./{category}") if os.path.isdir(f"{category}/{name}")]
-
-    for challenge in challenges:
-        if os.path.exists(f"{challenge}/challenge.yml"):
-            print(f"Syncing challenge: {challenge}")
-            os.system(f"ctf challenge sync '{challenge}'; ctf challenge install '{challenge}'")
-
-
-# Synchronize each category in it's own thread.
-if __name__ == "__main__":
-    visible = change_state(['wave1', 'wav'], 'visible')
-
-    init()
-    categories = get_categories()
-
-    jobs = []
-    for category in categories:
-        jobs.append(threading.Thread(target=sync, args=(category, )))
-    
-    for job in jobs:
-        job.start()
-
-    for job in jobs:
-        job.join()
-
-    print("Synchronized successfully!")
-    print("The following challenges are now visible:")
+    delete = """
+    gcloud compute firewall-rules delete chall-name
+    """
 
     for category in visible:
-        print(f"\n{category}:")
-        print('- ' + '\n- '.join([challenge for challenge in visible[category]]))
+        pass
+
+# # Initialize ctfcli with the CTFD_TOKEN and CTFD_URL.
+# def init():
+#     CTFD_TOKEN = os.getenv("CTFD_TOKEN", default=None)
+#     CTFD_URL = os.getenv("CTFD_URL", default=None)
+
+#     if not CTFD_TOKEN or not CTFD_URL:
+#         exit(1)
+
+#     os.system(f"echo '{CTFD_URL}\n{CTFD_TOKEN}\ny' | ctf init")
+
+
+# # Each category is in it's own directory, get the names of all directories that do not begin with '.'.
+# def get_categories():
+#     denylist_regex = r'\..*'
+
+#     categories = [name for name in os.listdir(".") if os.path.isdir(name) and not re.match(denylist_regex, name)]
+#     print("Categories: " + ", ".join(categories))
+#     return categories
+
+
+# # Synchronize all challenges in the given category, where each challenge is in it's own folder.
+# def sync(category):
+#     challenges = [f"{category}/{name}" for name in os.listdir(f"./{category}") if os.path.isdir(f"{category}/{name}")]
+
+#     for challenge in challenges:
+#         if os.path.exists(f"{challenge}/challenge.yml"):
+#             print(f"Syncing challenge: {challenge}")
+#             os.system(f"ctf challenge sync '{challenge}'; ctf challenge install '{challenge}'")
+
+
+# # Synchronize each category in it's own thread.
+if __name__ == "__main__":
+    visible = change_state(['wave1',], 'visible')
+
+#     init()
+#     categories = get_categories()
+
+#     jobs = []
+#     for category in categories:
+#         jobs.append(threading.Thread(target=sync, args=(category, )))
+    
+#     for job in jobs:
+#         job.start()
+
+#     for job in jobs:
+#         job.join()
+
+#     print("Synchronized successfully!")
+    # print("The following challenges are now visible:")
+
+    # for category in visible:
+    #     print(f"\n{category}:")
+    #     print('- ' + '\n- '.join([challenge for challenge in visible[category]]))
+    firewall(visible)
