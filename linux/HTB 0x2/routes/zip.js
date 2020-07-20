4a7fae0cf4f623a2e3d54adbdccaee03e0e1b343
thebongy <rishit.bansal0@gmail.com>
2020-07-20 19:37:17 +0530

    fix: Change path to admin

--- linux/HTB 0x2/routes/zip.js
+++ linux/HTB 0x2/routes/zip.js
@@ -3,12 +3,12 @@ const multer = require('multer');
 const fs = require('fs');
 const unzipper = require('unzipper');
 
-var upload = multer({ dest: '/home/csictf/uploads/zips' })
+var upload = multer({ dest: '/home/administrator/uploads/zips' })
 const router = express.Router();
 
 router.post('/extract', upload.single('zipFile'), (req, res, next) => {
     console.log(req.file);
-    fs.createReadStream(req.file.path).pipe(unzipper.Extract({ path: '/home/csictf/uploads/extracts' }));
+    fs.createReadStream(req.file.path).pipe(unzipper.Extract({ path: '/home/administrator/uploads/extracts' }));
     res.json({success: true});
 });
 
