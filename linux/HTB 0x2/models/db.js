7b084227efc857a8720355053f88ceb29ecd80ae
Rohan Mukherjee <rohanbayern@gmail.com>
2020-07-20 23:14:41 +0530

    feat: HTB0x6

--- linux/HTB 0x2/models/db.js
+++ linux/HTB 0x2/models/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
+// csictf{exp0s3d_sec23ts}
 mongoose.connect('mongodb://web:9EAC744765EA6F26@34.93.215.188:27017/HTBDB', {
     useNewUrlParser: true,
     useCreateIndex: true,
